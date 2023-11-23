02052372132a9e28fbd12d319a13059c85d353eb
Арслан Курбанов <arslik35@gmail.com>
2023-11-23 20:21:43 +0300

    dataBaseCreation

---
+++ sweethome-app/server/swetHome/Context/TroublesContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using swetHome.Models;
+
+namespace swetHome.Context
+{
+	public class TroublesContextDb : DbContext
+	{
+		public DbSet<Troubles>? Troubles { get; set; }
+
+		public TroublesContextDb(DbContextOptions<TroublesContextDb> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
---
+++ sweethome-app/server/swetHome/Context/UsersContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using swetHome.Models;
+
+namespace swetHome.Context
+{
+	public class UsersContextDb : DbContext
+	{
+		public DbSet<Users>? Users { get; set; }
+
+		public UsersContextDb(DbContextOptions<UsersContextDb> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
